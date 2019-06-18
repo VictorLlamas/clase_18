8f2d0d379c264b2c2485dc6fd55a0526d7a709d1
Victor Llamas Olivares <victorllamas@gmail.com>
2019-06-17 19:00:58 -0500

    public folder added, index.html with h1 tag added

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 9000
 
+
+app.use(express.static('public'));
+
 app.listen(port, function(req, res){
     console.log(`Server listening in port ${port}`);
 })
+
